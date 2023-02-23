82023f55965fecae16620d92b95a9a3d060c2813
Julian Chan Palomo <A20216390@alumnos.uady.mx>
2023-02-23 17:40:53 -0600

    Add javadoc files

---
+++ javadoc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"<Unnamed>","l":"DELink"},{"p":"<Unnamed>","l":"DELinkList"},{"p":"<Unnamed>","l":"DELinkListTest"}];updateSearchResults();
