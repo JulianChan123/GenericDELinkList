ee10aa2bda0e3f5e492fe78cd5ec43c429539263
Julian Chan Palomo <A20216390@alumnos.uady.mx>
2023-02-23 14:05:31 -0600

    DELinkList
    Normal DELinkList

---
+++ DELink.java
@@ -0,0 +1,12 @@
+public class DELink {
+    public double dData;
+    public DELink next; 
+
+    public DELink(double dd) {
+        dData = dd;
+    }
+
+    public void displayLink() {
+        System.out.print("{" + dData + "} ");
+    }
+}
---
+++ DELinkListTest.java
@@ -0,0 +1,16 @@
+public class DELinkListTest {
+    
+    public static void main(String[] args) {
+        DELinkList list = new DELinkList(); 
+        list.insertFirst(2.99);
+        list.insertFirst(4.99);
+        list.insertFirst(6.99);
+        list.insertFirst(8.99);
+        list.displayList(); 
+        
+        list.deleteFirst();
+        list.deleteFirst();    
+
+        list.displayList(); 
+    } 
+}
